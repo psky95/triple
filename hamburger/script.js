d8286e19f0204a225feb5e951155a11be5f723dc
psky95 <142555226+psky95@users.noreply.github.com>
2023-09-18 14:16:35 +0900

    Add files via upload

---
+++ hamburger/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    // 상단 홈페이지 햄버거 토글메뉴
+    $('.toggle-menu').click (function(){
+      $(this).toggleClass('active');
+      $('#menu').toggleClass('open');
+    });
+  
+  })
+  
